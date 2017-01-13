5e984dc4fff0375b2abde1b388e529962161143d
Luxlethal <succinto69@gmail.com>
2017-01-13 17:10:07 +0100

    UAUUUU
    OMG

--- login-action.php
+++ login-action.php
@@ -1,10 +1,19 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Documento senza titolo</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php
+if(!isset($_POST["username"]) || !isset($_POST["password"]) ) {
+	die("Errore! Parametri mancanti.");
+}
+$formato_campi = array("username" => "/^[a-zA-Z_\-{1,20}]$/",
+					  "password" => "/^[\W\w\s]{5,20}$/"
+				);
+$valido = true;
+foreach($formato_campi as $campo => $formato){
+	if(trim($_POST[$campo]) == "") {
+		echo "Compilare il campo $campo";
+		$valido = false;
+	}
+	elseif (!preg_match($formato, $_POST[$campo])){
+		echo "Formato non corretto per il campo $campo";
+		$valido = false;
+	}
+}
+?>
--- login.php
+++ login.php
@@ -2,9 +2,17 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Documento senza titolo</title>
+<title>Accedi all'area riservata</title>
 </head>
 
 <body>
+	<h1>Accedi all'area riservata</h1>
+    <form method="post" action="login-action.php">
+    	<input type="text" name="username" placeholder="Nome Utente">
+        <br>
+        <input type="password" name="password" placeholder="Password">
+        <br>
+        <input type="submit" value="Accedi">
+    </form>
 </body>
 </html>
